3478ac5218508e206c8a5bef34c735bfca53a96d
Dakkoku <106686342+Dakkoku@users.noreply.github.com>
2022-06-02 00:37:36 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("ÅZÅZÅZÅZÅZÅZÅZÅZÅZÅZ");
+			
+		int com = stdIn.nextInt();
+			
+		
+		System.exit(0);
+	}
+}
